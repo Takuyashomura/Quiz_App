a052e0697c9a0ec4ad3af70b108b0baccbd0c0d8
Takuyashomura <takudai1411@yahoo.co.jp>
2020-05-02 20:25:58 +0900

    Updates

---
+++ precache-manifest.04ceef11260232abbe029fd133209466.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e619e46a9b4407275abac42cbab4a9ff",
+    "url": "/Quiz_App/index.html"
+  },
+  {
+    "revision": "fd13db244a36a36582fd",
+    "url": "/Quiz_App/static/css/main.55a98dba.chunk.css"
+  },
+  {
+    "revision": "7ad12c5ab98320e18e35",
+    "url": "/Quiz_App/static/js/2.01e56e95.chunk.js"
+  },
+  {
+    "revision": "1b0e9eedc9c6ce185e4815b502179b21",
+    "url": "/Quiz_App/static/js/2.01e56e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd13db244a36a36582fd",
+    "url": "/Quiz_App/static/js/main.b0aa884c.chunk.js"
+  },
+  {
+    "revision": "f74eaad80958a6f57b10",
+    "url": "/Quiz_App/static/js/runtime-main.6fad673a.js"
+  }
+]);
